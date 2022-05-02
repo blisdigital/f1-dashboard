bfde02e58b96fb46c85a1797228494b40d2bb55e
Patrick van der Linden <p.vanderlinden@weareblis.com>
2022-05-02 11:48:12 +0200

    Add round 2 Imola and insert the correct start end end dates

--- races.js
+++ races.js
@@ -1,3 +1,4 @@
 const races = [
-    { name: "australia", title: "1. Australië", start: "2022-04-13T08:00:00.000+02:00", end: "2022-05-01T23:59:59.000+02:00" }
+    { name: "australia", title: "1. Australië", start: "2022-04-13T08:00:00.000+02:00", end: "2022-04-24T23:59:59.000+02:00" },
+    { name: "imola", title: "2. Imola", start: "2022-04-25T08:00:00.000+02:00", end: "2022-05-08T23:59:59.000+02:00" }
 ];
