9214b937b30b3670caa061aa1352ffbdda80d9f0
Patrick van der Linden <p.vanderlinden@weareblis.com>
2022-05-02 11:47:44 +0200

    Add drivers for the 2022 F1 Challenge

--- drivers.js
+++ drivers.js
@@ -1,5 +1,52 @@
 const drivers = {
-    "Firstname Lastname": {
+    "Eric Dubbelaar": {
         league: Leagues.FirstTimer
     },
+    "Roberto Soebhan": {
+        league: Leagues.FirstTimer
+    },
+    "Ton van Tilburg": {
+        league: Leagues.FirstTimer
+    },
+    "Eric Kruithof": {
+        league: Leagues.FirstTimer
+    },
+    "Peter Hogenbirk": {
+        league: Leagues.Beginner
+    },
+    "Arco van der Velde": {
+        league: Leagues.Beginner
+    },
+    "Menno van der Zwart": {
+        league: Leagues.Beginner
+    },
+    "Kevin Rutte": {
+        league: Leagues.Beginner
+    },
+    "Christian Boer": {
+        league: Leagues.Amateur
+    },
+    "Jasper Hopstaken": {
+        league: Leagues.Amateur
+    },
+    "Richard Dekker": {
+        league: Leagues.Amateur
+    },
+    "Benjamin Dudok": {
+        racingNumber: 24,
+        league: Leagues.Experienced
+    },
+    "Pelle Overbeeke": {
+        racingNumber: 11,
+        league: Leagues.Experienced
+    },
+    "Cees Schrijen": {
+        league: Leagues.Experienced
+    },
+    "Dirk-Jan Los": {
+        league: Leagues.Experienced
+    },
+    "Richard Schot": {
+        league: Leagues.Experienced
+    }
 };
