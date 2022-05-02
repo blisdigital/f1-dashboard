ded64d2fb17386af1a91db36fcfea10bc5eb22fb
Patrick van der Linden <p.vanderlinden@weareblis.com>
2022-05-02 11:48:31 +0200

    Update racing results

--- results/1-australia.js
+++ results/1-australia.js
@@ -1,6 +1,81 @@
 results["australia"] = [{
-    driver: "Firstname Lastname",
+    driver: "Eric Dubbelaar",
     simulator: Simulators.BlisLefty,
-    time: "1:30.000",
+    time: "1:26.634",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Roberto Soebhan",
+    simulator: Simulators.TheRightOne,
+    time: "1:27.155",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Ton van Tilburg",
+    simulator: Simulators.BlisLefty,
+    time: "1:27.946",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Eric Kruithof",
+    simulator: Simulators.TheRightOne,
+    time: "1:28.507",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Peter Hogenbirk",
+    simulator: Simulators.BlisLefty,
+    time: "1:29.194",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Arco van der Velde",
+    simulator: Simulators.TheRightOne,
+    time: "1:25.854",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Menno van der Zwart",
+    simulator: Simulators.BlisLefty,
+    time: "1:31.922",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Kevin Rutte",
+    simulator: Simulators.TheRightOne,
+    time: "1:28.204",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Christian Boer",
+    simulator: Simulators.BlisLefty,
+    time: "1:27.247",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Jasper Hopstaken",
+    simulator: Simulators.TheRightOne,
+    time: "1:30.441",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Richard Dekker",
+    simulator: Simulators.BlisLefty,
+    time: "",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Benjamin Dudok",
+    simulator: Simulators.BlisLefty,
+    time: "",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Pelle Overbeeke",
+    simulator: Simulators.TheRightOne,
+    time: "",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Cees Schrijen",
+    simulator: Simulators.TheRightOne,
+    time: "1:25.497",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Dirk-Jan Los",
+    simulator: Simulators.BlisLefty,
+    time: "1:28.629",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Richard Schot",
+    simulator: Simulators.BlisLefty,
+    time: "",
     tyre: TyreCompounds.Soft,
 }];
