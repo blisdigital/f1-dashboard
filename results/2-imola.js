0768c1cddfd96ec9cd67e73e61e4459fd6b2e013
Patrick van der Linden <patrick@weareblis.com>
2022-05-17 14:00:09 +0200

    Update Imola results

--- results/2-imola.js
+++ results/2-imola.js
@@ -1,1 +1,81 @@
-results["imola"] = [];
+results["imola"] = [{
+    driver: "Eric Dubbelaar",
+    simulator: Simulators.BlisLefty,
+    time: "1:21.325",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Roberto Soebhan",
+    simulator: Simulators.TheRightOne,
+    time: "1:21.631",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Ton van Tilburg",
+    simulator: Simulators.BlisLefty,
+    time: "1:19.709",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Erik Kruithof",
+    simulator: Simulators.TheRightOne,
+    time: "1:22.239",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Peter Hogenbirk",
+    simulator: Simulators.TheRightOne,
+    time: "1:19.929",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Arco van der Velde",
+    simulator: Simulators.TheRightOne,
+    time: "DNS",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Menno van der Zwart",
+    simulator: Simulators.TheRightOne,
+    time: "1:21.736",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Kevin Rutten",
+    simulator: Simulators.BlisLefty,
+    time: "1:18.491",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Christian Boer",
+    simulator: Simulators.BlisLefty,
+    time: "DNS",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Jasper Hopstaken",
+    simulator: Simulators.TheRightOne,
+    time: "DNS",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Richard Dekker",
+    simulator: Simulators.BlisLefty,
+    time: "1:21.272",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Benjamin Dudok",
+    simulator: Simulators.TheRightOne,
+    time: "1:19.557",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Pelle Overbeeke",
+    simulator: Simulators.BlisLefty,
+    time: "1:20.793",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Cees Schrijen",
+    simulator: Simulators.TheRightOne,
+    time: "DNS",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Dirk-Jan Los",
+    simulator: Simulators.BlisLefty,
+    time: "1:21.606",
+    tyre: TyreCompounds.Soft,
+}, {
+    driver: "Richard Schot",
+    simulator: Simulators.TheRightOne,
+    time: "DNS",
+    tyre: TyreCompounds.Soft,
+}];
